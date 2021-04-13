Fix mean/sigma calculation

main.py
@@ -2,7 +2,6 @@
 # Reference:
 # https://towardsdatascience.com/simulating-stock-prices-in-python-using-geometric-brownian-motion-8dfd6e8c6b18
 
-import pandas as pd
 import numpy as np
 from Stock import Stock
 import matplotlib.pyplot as plt
@@ -20,14 +19,17 @@ SCENARIOS = 10
 msft = Stock('MSFT')
 msft.get(START_DATE, END_DATE)
 
+pred_start_index = get_index(msft.dates, pd.to_datetime(PRED_START_DATE, format="%Y-%m-%d"))
+pred_end_index = get_index(msft.dates, pd.to_datetime(PRED_END_DATE, format="%Y-%m-%d"))
+
 
 s0 = msft.closes[0]
 dt = 1
 T = get_weekdays_between(PRED_START_DATE, PRED_END_DATE)
 N = T / dt
 t = np.arange(1, int(N)+1)
-mu = np.mean(msft.returns)  # TODO: only calc up to pred start!
-sigma = np.std(msft.returns)
+mu = np.mean(msft.returns[:pred_start_index])
+sigma = np.std(msft.returns[:pred_start_index])
 dW_scenarios = {str(scenario_num): np.random.normal(0, 1, int(N)) for scenario_num in range(1, SCENARIOS + 1)}
 W_scenarios = {str(scenario_num): dW_scenarios[str(scenario_num)].cumsum() for scenario_num in range(1, SCENARIOS + 1)}
 