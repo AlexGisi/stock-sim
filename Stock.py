03d117f3ff932827d7b596e220c6d206ce83d5e3
Alex Gisi <apgisi11@gmail.com>
2021-04-13 11:26:58 -0500

    Implement Stock and model parameters

---
+++ Stock.py
@@ -0,0 +1,39 @@
+import quandl
+import matplotlib.pyplot as plt
+from pandas.plotting import register_matplotlib_converters
+
+register_matplotlib_converters()
+
+QUANDL_AUTH = 'fdcJB3tASsz6tujQXJoQ'
+
+
+class Stock:
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.data_start = None
+        self.data_end = None
+        self.dates = None
+        self.closes = None
+        self.returns = None
+
+    def get(self, start, end):
+        self.data_start = start
+        self.data_end = end
+
+        data = quandl.get("EOD/" + self.ticker,
+                          authtoken=QUANDL_AUTH,
+                          start_date=start,
+                          end_date=end,
+                          ).reset_index(drop=False)[['Date', 'Close']]
+        self.dates = data['Date']
+        self.closes = data['Close']
+
+        self.returns = ((self.closes - self.closes.shift(1)) / self.closes.shift(1)).tolist()
+        self.returns[0] = 0
+
+    def plot_full(self):
+        plt.figure(figsize=(15, 5))
+        plt.plot(self.dates, self.closes)
+        plt.xlabel('Days')
+        plt.ylabel(self.ticker + ' Price')
+        plt.show()
--- main.py
+++ main.py
@@ -1,21 +1,44 @@
+# Using data from start to before 2017-10-1, simulate until 2017-12-28.
+# Reference:
+# https://towardsdatascience.com/simulating-stock-prices-in-python-using-geometric-brownian-motion-8dfd6e8c6b18
+
 import pandas as pd
 import numpy as np
-import quandl
-import matplotlib.pyplot as plt
-from pandas.plotting import register_matplotlib_converters
+from Stock import Stock
 
-register_matplotlib_converters()
-QUANDL_AUTH = "fdcJB3tASsz6tujQXJoQ"
+START_DATE = '2015-12-28'
+END_DATE = '2017-12-28'
+PRED_START_DATE = '2017-10-1'
+PRED_END_DATE = '2017-12-28'
+SCENARIOS = 10
 
-msft = quandl.get("EOD/MSFT",
-                  authtoken="fdcJB3tASsz6tujQXJoQ",
-                  start_date='2015-12-28',
-                  end_date='2017-12-28'
-                  ).reset_index(drop=False)[['Date', 'Close']]
+msft = Stock('MSFT')
+msft.get(START_DATE, END_DATE)
 
 
-plt.figure(figsize=(15, 5))
-plt.plot(msft['Date'], msft['Close'])
-plt.xlabel('Days')
-plt.ylabel('MSFT Price')
-plt.show()
+def get_weekdays_between(start, end):
+    return pd.date_range(start=pd.to_datetime(start,
+                         format="%Y-%m-%d") + pd.Timedelta('1 days'),
+                         end=pd.to_datetime(end,
+                         format="%Y-%m-%d")).to_series(
+                         ).map(lambda x:
+                               1 if x.isoweekday() in range(1, 6) else 0).sum()
+
+
+# msft.plot_full()
+
+
+s0 = msft.closes[0]
+dt = 1
+T = get_weekdays_between(PRED_START_DATE, PRED_END_DATE)
+N = T / dt
+t = np.arange(1, int(N) + 1)
+mu = np.mean(msft.returns)
+sigma = np.std(msft.returns)
+dW_scenarios = {str(scenario_num): np.random.normal(0, 1, int(N)) for scenario_num in range(1, SCENARIOS + 1)}
+W_scenarios = {str(scenario_num): dW_scenarios[str(scenario_num)].cumsum() for scenario_num in range(1, SCENARIOS + 1)}
+
+print(mu)
+print(sigma)
+print(dW_scenarios['1'])
+print(W_scenarios['1'])
